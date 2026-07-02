2946511558cf0e49ba6cd89c85174a2976eb348f
Jeniffer Carrillo Ponton <jcarrillo1@byupathway.edu>
2026-07-02 00:57:23 -0500

    Complete week 01 exercises

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+        int largestNumber = numbers[0];
+        int smallestPositiveNumber = 0;
+
+        foreach (int currentNumber in numbers)
+        {
+            sum += currentNumber;
+
+            if (currentNumber > largestNumber)
+            {
+                largestNumber = currentNumber;
+            }
+
+            if (currentNumber > 0)
+            {
+                if (smallestPositiveNumber == 0 || currentNumber < smallestPositiveNumber)
+                {
+                    smallestPositiveNumber = currentNumber;
+                }
+            }
+        }
+
+        double average = ((double)sum) / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average.ToString(CultureInfo.InvariantCulture)}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+
+        numbers.Sort();
+
+        Console.WriteLine("The sorted list is:");
+
+        foreach (int currentNumber in numbers)
+        {
+            Console.WriteLine(currentNumber);
+        }
     }
 }
